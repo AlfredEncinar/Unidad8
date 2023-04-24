6aa77eb7e782e00662b92bd2546aea38078a1f6d
Alfred Encinar <encinar101@gmail.com>
2023-04-24 17:22:51 +0200

    g

--- src/main/java/org/example/Ejercicio1.java
+++ src/main/java/org/example/Ejercicio1.java
@@ -1,4 +1,6 @@
 package org.example;
+//Realiza un programa para calcular el área de un triángulo según su base y su altura. La interfaz debe ser como la que se muestra a continuación y responder de un modo análogo.
+//Crea además test en JUnit para comprobar que todo funciona correctamente. Recuerda lo que vimos en entornos de desarrollo a la hora de diseñar test.
 
 public class Ejercicio1 {
 }
